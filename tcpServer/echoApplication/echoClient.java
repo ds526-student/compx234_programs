a75209956d1c7cc9aca5ba0f4821d7ab25251632
Damion <ds526@students.waikato.ac.nz>
2025-04-13 14:27:51 +1200

    basic tcp server created

--- tcpServer/echoApplication/echoClient.java
+++ tcpServer/echoApplication/echoClient.java
@@ -1,5 +1,17 @@
+
+import java.net.Socket;
+
 public class echoClient {
-    public static void Main(String[] args){
-        
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client Started");
+            Socket soc = new Socket("localhost", 52712);
+            System.out.println("Connected to server: " + soc.getInetAddress().getHostAddress() + ":" + soc.getPort());
+
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
--- tcpServer/echoApplication/echoServer.java
+++ tcpServer/echoApplication/echoServer.java
@@ -1,5 +1,21 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class echoServer {
-    public static void Main(String[] args){
-        
+    public static void main(String[] args) {
+
+        try{
+            System.out.println("Waiting for clients...");
+            ServerSocket ss = new ServerSocket(52712);
+            Socket soc = ss.accept();
+            System.out.println("Client connected: " + soc.getInetAddress().getHostAddress() + ":" + soc.getPort());
+
+            
+        }
+        catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 }
+
